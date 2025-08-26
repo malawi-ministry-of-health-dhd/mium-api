f3f704e7a41a720e3ce88410b43169fd3c8d8ddb
Louis Nyirongo <louisnyirongo73@gmail.com>
2025-08-26 16:04:09 +0200

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,19 +5,25 @@ import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule,{cors:false});
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.setGlobalPrefix('api');
+
+  app.enableCors({
+    origin: '*', // allow all
+  });
 
   // 1️⃣ Configure Swagger
   const config = new DocumentBuilder()
     .setTitle('MAHIS integrated User Management(MIUM) Auth API')
-    .setDescription('API documentation for NestJS app with Prisma, JWT, Roles, Programs')
+    .setDescription(
+      'API documentation for NestJS app with Prisma, JWT, Roles, Programs',
+    )
     .setVersion('1.0')
     .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'JWT' }) // JWT auth
     .build();
 
-      // Serve static files
+  // Serve static files
   app.useStaticAssets(join(__dirname, '..', 'public'));
 
   // 2️⃣ Create document
@@ -26,8 +32,6 @@ async function bootstrap() {
   // 3️⃣ Setup Swagger UI endpoint
   SwaggerModule.setup('api/docs', app, document);
 
-   await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
-
-
