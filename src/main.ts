4bb5c7542393868d8a5cb41ba274cf0e0fe80be3
Louis Nyirongo <louisnyirongo73@gmail.com>
2025-08-26 15:30:29 +0200

    disable cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule,{cors:false});
 
   app.setGlobalPrefix('api');
 
